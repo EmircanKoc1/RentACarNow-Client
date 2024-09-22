3cbed332585946e93e9b39592b6addf7b477588b
Emircan <emircandv787@gmail.com>
2024-09-23 01:43:53 +0300

    The services have been added to the DI container.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { ClaimService } from './core/services/implementations/claim.service';
+import { RentalService } from './core/services/implementations/rental.service';
+import { BrandService } from './core/services/implementations/brand.service';
+import { UserService } from './core/services/implementations/user.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
      provideHttpClient(),
-     ClaimService
+     ClaimService,
+     RentalService,
+     BrandService,
+     UserService,
+     RentalService
     ]
 };
