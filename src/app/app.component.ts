39ff8b33ca18662969609b39c39f0f5fd512a116
Emircan <emircandv787@gmail.com>
2024-11-16 01:42:59 +0300

    The prime ng has been updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClaimService } from './core/services/implementations/claim.service';
 import { IGetAllQueryParam } from './shared/models/get-all-query-param.model';
+import { CarService } from './core/services/implementations/car.service';
+import { BrandService } from './core/services/implementations/brand.service';
+import { UserService } from './core/services/implementations/user.service';
+import { RentalService } from './core/services/implementations/rental.service';
+import { IClaimQueryResponse } from './shared/models/claim/get-claim.response.model';
+import { IResponseWrapper } from './shared/models/response-wrapper.model';
+import { IPaginationInfo } from './shared/models/pagination-info.model';
 
 
 @Component({
@@ -14,21 +21,44 @@ import { IGetAllQueryParam } from './shared/models/get-all-query-param.model';
 export class AppComponent {
   title = 'RentACarNow';
 
-  private claimService:ClaimService;
-  
+  private claimService: ClaimService;
+  private rentalService: RentalService;
+  private userService: UserService;
+  private brandService: BrandService;
+  private carService: CarService;
+
+  public claims: IResponseWrapper<IClaimQueryResponse[]>;
+
   constructor() {
     this.claimService = inject(ClaimService);
-    
-    const queryParam: IGetAllQueryParam = {
-        isAscending :true,
-        sortingField:"",
-        pageNumber:1,
-        pageSize:10
+    this.rentalService = inject(RentalService);
+    this.userService = inject(UserService);
+    this.brandService = inject(BrandService);
+    this.carService = inject(CarService);
+
+    const paginationInfo: IPaginationInfo = {
+      pageNumber: 1,
+      pageItemSize: 100,
+      totalItemCount: 100,
+      totalPageCount: 100
     }
 
-    this.claimService.getAll("/api/v1/Claim/GetAll",queryParam)
-    .subscribe(response => console.log(response));
-  
+    const claimQueryResponse: IClaimQueryResponse[] = [];
+
+    const fakeClaims: IResponseWrapper<IClaimQueryResponse[]> = {
+      httpStatusCode: 200,
+      errorMessages: [],
+      data: claimQueryResponse,
+      paginationInfo: paginationInfo,
+    };
+
+    this.claims = fakeClaims;
+
+    this.claimService.getAll().subscribe(response => {
+      console.log(response);
+      this.claims = response;
+    });
+
   }
-  
+
 }
